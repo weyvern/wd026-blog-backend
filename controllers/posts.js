3df9e1306947bed20dfdec8dd839545f0c1e848a
Jorge Paul <jserranopaul@gmail.com>
2022-05-27 12:39:39 +0200

    error handling

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,62 +1,44 @@
 import Post from '../models/Post.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import ErrorResponse from '../utils/ErrorResponse.js';
 
-export const getAllPosts = async (req, res, next) => {
-  try {
-    const posts = await Post.find();
-    res.json(posts);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+export const getAllPosts = asyncHandler(async (req, res, next) => {
+  const posts = await Post.find();
+  res.json(posts);
+});
 
-export const createPost = async (req, res) => {
-  try {
-    const { body } = req;
-    const newPost = await Post.create({ ...body });
-    res.status(201).json(newPost);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+export const createPost = asyncHandler(async (req, res, next) => {
+  const { body } = req;
+  const newPost = await Post.create({ ...body });
+  res.status(201).json(newPost);
+});
 
-export const getSinglePost = async (req, res) => {
-  try {
-    const {
-      params: { id }
-    } = req;
-    const post = await Post.findById(id);
-    if (!post) throw new Error(`Post with id of ${id} doesn't exist`);
-    res.send(post);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+export const getSinglePost = asyncHandler(async (req, res, next) => {
+  const {
+    params: { id }
+  } = req;
+  const post = await Post.findById(id);
+  if (!post) throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  res.send(post);
+});
 
-export const updatePost = async (req, res) => {
-  try {
-    const {
-      body,
-      params: { id }
-    } = req;
-    const found = await Post.findById(id);
-    if (!found) throw new Error(`Post with id of ${id} doesn't exist`);
-    const updatedPost = await Post.findOneAndUpdate({ _id: id }, body, { new: true });
-    res.json(updatedPost);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+export const updatePost = asyncHandler(async (req, res, next) => {
+  const {
+    body,
+    params: { id }
+  } = req;
+  const found = await Post.findById(id);
+  if (!found) throw new Error(`Post with id of ${id} doesn't exist`);
+  const updatedPost = await Post.findOneAndUpdate({ _id: id }, body, { new: true });
+  res.json(updatedPost);
+});
 
-export const deletePost = async (req, res) => {
-  try {
-    const {
-      params: { id }
-    } = req;
-    const found = await Post.findById(id);
-    if (!found) throw new Error(`Post with id of ${id} doesn't exist`);
-    await Post.deleteOne({ _id: id });
-    res.json({ success: `Post with id of ${id} was deleted` });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+export const deletePost = asyncHandler(async (req, res, next) => {
+  const {
+    params: { id }
+  } = req;
+  const found = await Post.findById(id);
+  if (!found) throw new Error(`Post with id of ${id} doesn't exist`);
+  await Post.deleteOne({ _id: id });
+  res.json({ success: `Post with id of ${id} was deleted` });
+});
